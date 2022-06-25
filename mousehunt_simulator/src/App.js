fc1c7ac059d0b8acfdeb4c46e201660fd520837c
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2022-06-25 19:26:17 +0800

    Implementation of the button as a toggle for the simulation.

--- mousehunt_simulator/src/App.js
+++ mousehunt_simulator/src/App.js
@@ -1,9 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 import EntryTable from './components/EntryTable';
 import ControlPanel from './components/ControlPanel';
+import Button from './components/Button'
 
 function App() {
+  const [SimulateNow, setSimulate] = useState(false);
+  const [Buttontext, setText] = useState('Simulate');
+  
+  function toggle(event) {
+    if (SimulateNow === true) {
+      setSimulate(false);
+      setText('Simulate');
+    } else {
+      setSimulate(true);
+      setText('Reset');
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +27,8 @@ function App() {
           Edit the simulator values and click on the simulate button to simulate hunts
         </p>
         <ControlPanel></ControlPanel>
-        <EntryTable huntTill={30} cheese={'brie'} power={3000} luck={30} powerType={'shadow'} location={'Meadow'}/>
+        <Button Purpose={Buttontext} do={toggle}></Button>
+        <EntryTable display={SimulateNow} huntTill={30} cheese={'brie'} power={3000} luck={30} powerType={'shadow'} location={'Meadow'}/>
       </header>
     </div>
   );
--- mousehunt_simulator/src/components/EntryTable.js
+++ mousehunt_simulator/src/components/EntryTable.js
@@ -1,17 +1,28 @@
-import {DataTable} from './operations'
+import {DataTable} from './operations';
 import Entry from "./Entry";
+import {useState} from 'react';
 
 function EntryTable(props) {
     let table = new DataTable(props.cheese, props.power, props.luck, props.powerType, props.location);
-    let entries = [];
-    for (let i = 1; i <= props.huntTill; i++) {
-        entries.push(<Entry table={table} huntno = {i} key={i} />)
-    }
-    return (
+    
+    if (props.display === true) {
+        let entries = [];
+        for (let i = 1; i <= props.huntTill; i++) {
+            entries.push(<Entry table={table} huntno = {i} key={i} />)
+        }
+        return (
         <div className='EntryTable'>
             {entries}
         </div>
-    );
+        );
+    } else {
+        return (
+            <div className='EntryTable'>
+                
+            </div>
+        )
+    }
+    
 }
 
 export default EntryTable;
